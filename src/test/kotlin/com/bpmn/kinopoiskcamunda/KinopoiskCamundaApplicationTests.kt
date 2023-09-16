d11bbf41bcd3e6ad76aae6c015b0bc0f92b37cd8
Admin <admin@MacBook-Pro-Admin.local>
2023-09-16 10:24:04 +0300

    Initial commit

---
+++ src/test/kotlin/com/bpmn/kinopoiskcamunda/KinopoiskCamundaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bpmn.kinopoiskcamunda
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KinopoiskCamundaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
